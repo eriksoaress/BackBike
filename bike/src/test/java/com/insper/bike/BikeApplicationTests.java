aed85b1432c7ce1b4ab47638b11055b560c5993d
GustavoAntony <105286051+GustavoAntony@users.noreply.github.com>
2023-04-27 11:24:52 -0300

    Add files via upload

---
+++ bike/src/test/java/com/insper/bike/BikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insper.bike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
