e9256196e689f5fcd1b90b7fcda26b69aab138d5
GustavoAntony <gustavoa9@al.insper.edu.br>
2023-05-02 11:11:22 -0300

    bike cadastro done

--- bike/src/main/java/com/insper/bike/bike/BikeController.java
+++ bike/src/main/java/com/insper/bike/bike/BikeController.java
@@ -1,4 +1,22 @@
 package com.insper.bike.bike;
 
+import com.insper.bike.bike.dto.BikeReturnDTO;
+import com.insper.bike.bike.dto.SaveBikeDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bike")
 public class BikeController {
+    @Autowired
+    private BikeService bikeService;
+    @PostMapping
+    public BikeReturnDTO saveBike(@RequestBody SaveBikeDTO bike){
+        return bikeService.saveBike(bike);
+    }
+
 }
