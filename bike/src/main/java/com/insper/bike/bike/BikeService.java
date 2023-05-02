01507c6153c6e0dee8861d0386cf38c9a8cf38e7
GustavoAntony <gustavoa9@al.insper.edu.br>
2023-05-02 11:06:37 -0300

    bike set

--- bike/src/main/java/com/insper/bike/bike/BikeService.java
+++ bike/src/main/java/com/insper/bike/bike/BikeService.java
@@ -1,5 +1,7 @@
 package com.insper.bike.bike;
 
+import com.insper.bike.bike.dto.BikeReturnDTO;
+import com.insper.bike.bike.dto.SaveBikeDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +10,16 @@ public class BikeService {
     @Autowired
     private BikeRepository bikeRepository;
 
-    public Bike saveBike(Bike bike){
-        return  null;
+    public BikeReturnDTO saveBike(SaveBikeDTO saveBikeDTO){
+
+        Bike bike = new Bike();
+        bike.setModel(saveBikeDTO.getModel());
+        bike.setType(saveBikeDTO.getType());
+        bike.setPrice_p_hour(saveBikeDTO.getPrice_p_hour());
+        bike.setStatus_ocupation(BikeStatusOcupation.AVAILABLE);
+        bike.setStatus_util(BikeStatusUtil.WORKING);
+        bikeRepository.save(bike);
+        return BikeReturnDTO.convert(bike);
     }
 
 }
--- bike/src/main/java/com/insper/bike/bike/dto/BikeReturnDTO.java
+++ bike/src/main/java/com/insper/bike/bike/dto/BikeReturnDTO.java
@@ -1,5 +1,9 @@
 package com.insper.bike.bike.dto;
 
+import com.insper.bike.bike.Bike;
+import com.insper.bike.bike.BikeRepository;
+import com.insper.bike.bike.BikeStatusOcupation;
+import com.insper.bike.bike.BikeStatusUtil;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,4 +13,16 @@ public class BikeReturnDTO {
     private String model;
     private String type;
     private Float price_p_hour;
+    private BikeStatusUtil status_util;
+    private BikeStatusOcupation status_ocupation;
+
+    public static BikeReturnDTO convert(Bike bike) {
+        BikeReturnDTO bikeReturnDTO = new BikeReturnDTO();
+        bikeReturnDTO.setModel(bike.getModel());
+        bikeReturnDTO.setType(bike.getType());
+        bikeReturnDTO.setPrice_p_hour(bike.getPrice_p_hour());
+        bikeReturnDTO.setStatus_util(bike.getStatus_util());
+        bikeReturnDTO.setStatus_ocupation(bike.getStatus_ocupation());
+        return bikeReturnDTO;
+    }
 }
